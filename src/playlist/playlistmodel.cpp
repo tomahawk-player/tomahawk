fd0a27fc7c1be296cd381ac65a3622c9672290f9
Christian Muehlhaeuser <muesli@gmail.com>
2010-10-24 05:01:51 +0200

    * Don't try removing rows when the model is empty.

--- src/playlist/playlistmodel.cpp
+++ src/playlist/playlistmodel.cpp
@@ -47,10 +47,13 @@ PlaylistModel::loadPlaylist( const Tomahawk::playlist_ptr& playlist )
     if ( !m_playlist.isNull() )
         disconnect( m_playlist.data(), SIGNAL( revisionLoaded( Tomahawk::PlaylistRevision ) ), this, SLOT( onRevisionLoaded( Tomahawk::PlaylistRevision ) ) );
 
-    emit beginRemoveRows( QModelIndex(), 0, rowCount( QModelIndex() ) - 1 );
-    delete m_rootItem;
-    m_rootItem = new PlItem( 0, this );
-    emit endRemoveRows();
+    if ( rowCount( QModelIndex() ) )
+    {
+        emit beginRemoveRows( QModelIndex(), 0, rowCount( QModelIndex() ) - 1 );
+        delete m_rootItem;
+        m_rootItem = new PlItem( 0, this );
+        emit endRemoveRows();
+    }
 
     m_playlist = playlist;
     connect( playlist.data(), SIGNAL( revisionLoaded( Tomahawk::PlaylistRevision ) ), SLOT( onRevisionLoaded( Tomahawk::PlaylistRevision ) ) );
