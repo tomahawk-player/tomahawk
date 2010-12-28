8b9b02e15dc3e5e7855bbc512dfc2e320c85b039
Christian Muehlhaeuser <muesli@gmail.com>
2010-12-28 06:23:15 +0100

    * Mentioning Alejandro (xevix) in the About Dialog.

--- src/tomahawkwindow.cpp
+++ src/tomahawkwindow.cpp
@@ -389,5 +389,5 @@ TomahawkWindow::setWindowTitle( const QString& title )
 void
 TomahawkWindow::showAboutTomahawk()
 {
-    QMessageBox::about( this, "About Tomahawk", "Copyright 2010 Christian Muehlhaeuser <muesli@gmail.com>\n\nThanks to: Leo Franchi, Jeff Mitchell, Dominik Schmidt and Steve Robertson" );
+    QMessageBox::about( this, "About Tomahawk", "Copyright 2010 Christian Muehlhaeuser <muesli@gmail.com>\n\nThanks to: Leo Franchi, Jeff Mitchell, Dominik Schmidt, Alejandro Wainzinger and Steve Robertson" );
 }
