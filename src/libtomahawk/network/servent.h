049b8381f0b81c4b3f08b3f39d99f6842a2c8f8b
Christian Muehlhaeuser <muesli@gmail.com>
2011-01-23 05:01:44 +0100

    * Increased idle timeout.

--- src/libtomahawk/network/servent.h
+++ src/libtomahawk/network/servent.h
@@ -4,7 +4,7 @@
 // port for servent to listen on
 #define DEFAULT_LISTEN_PORT 50210
 // time before new connection terminates if no auth received
-#define AUTH_TIMEOUT 15000
+#define AUTH_TIMEOUT 180000
 
 #include <QObject>
 #include <QTcpServer>
