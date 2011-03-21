aedebfe9a9e41f2cdc6e1cc50bdc24046692af52
Christian Muehlhaeuser <muesli@gmail.com>
2011-03-21 09:21:58 +0100

    * APPLICATION_NAME is now Tomahawk instead of Player. A few more build system fixes.

--- admin/mac/build-release-osx.sh
+++ admin/mac/build-release-osx.sh
@@ -48,8 +48,9 @@ CREATEDMG='1'
     header "Running install_name_tool"
     $ROOT/../admin/mac/deposx.sh
 
-    header "Renaming icon & copying Info.plist"
+    header "Renaming files & copying Info.plist"
     mv Contents/Resources/tomahawkSources.icns Contents/Resources/Tomahawk.icns
+    mv Contents/MacOS/tomahawk Contents/MacOS/Tomahawk
     cp $ROOT/../admin/mac/Info.plist Contents/Info.plist
 
     header "Copying Sparkle pubkey & framework, and qt.conf"
