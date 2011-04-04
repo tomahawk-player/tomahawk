16eb50e02da824764ba55ae36ddd6c5a9c0ff1d2
Dominik Schmidt <dev@dominik-schmidt.de>
2011-04-04 20:46:21 +0200

    Forgot to add tomahawksipmessage stanza extension files

---
+++ src/sip/jreen/tomahawksipmessage.h
@@ -0,0 +1,28 @@
+#ifndef ENTITYTIME_H
+#define ENTITYTIME_H
+
+#include <jreen/stanzaextension.h>
+
+#define TOMAHAWK_SIP_MESSAGE_NS QLatin1String("http://www.tomhawk-player.org/sip/transports")
+
+class TomahawkSipMessagePrivate;
+class TomahawkSipMessage : public Jreen::StanzaExtension
+{
+    J_EXTENSION(TomahawkSipMessage, "")
+    Q_DECLARE_PRIVATE(TomahawkSipMessage)
+    public:
+        TomahawkSipMessage(QString ip, unsigned int port, QString uniqname, QString key, bool visible);
+        // sets visible to false as we dont have any extra information
+        TomahawkSipMessage();
+        ~TomahawkSipMessage();
+
+        const QString ip() const;
+        unsigned int port() const;
+        QString uniqname() const;
+        QString key() const;
+        bool visible() const;
+    private:
+        QScopedPointer<TomahawkSipMessagePrivate> d_ptr;
+};
+
+#endif // ENTITYTIME_H
