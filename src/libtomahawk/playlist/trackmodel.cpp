ab2443792ac24d39be185d93fe34162fe2aa1edc
Christian Muehlhaeuser <muesli@gmail.com>
2011-04-06 06:18:00 +0200

    * Don't manually delete the root item. This speeds up shut down, since the proxy will be disconnected before the root item gets deleted (automatically).

--- src/libtomahawk/playlist/trackmodel.cpp
+++ src/libtomahawk/playlist/trackmodel.cpp
@@ -1,5 +1,5 @@
 /* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
- * 
+ *
  *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
  *
  *   Tomahawk is free software: you can redistribute it and/or modify
@@ -45,7 +45,7 @@ TrackModel::TrackModel( QObject* parent )
 
 TrackModel::~TrackModel()
 {
-    delete m_rootItem;
+//    delete m_rootItem;
 }
 
 
