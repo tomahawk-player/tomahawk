66e3d07aee1ef0145191b170a2d947efa553ca31
Christian Muehlhaeuser <muesli@gmail.com>
2011-04-15 07:50:38 +0200

    * Use canonicalPath() wherever dealing with file- & dir-paths.

--- src/libtomahawk/database/databasecommand_dirmtimes.cpp
+++ src/libtomahawk/database/databasecommand_dirmtimes.cpp
@@ -1,5 +1,5 @@
 /* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
- * 
+ *
  *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
  *
  *   Tomahawk is free software: you can redistribute it and/or modify
@@ -60,7 +60,7 @@ DatabaseCommand_DirMtimes::execSelectPath( DatabaseImpl *dbi, const QDir& path, 
                             "FROM dirs_scanned "
                             "WHERE name LIKE :prefix" ) );
 
-    query.bindValue( ":prefix", path.absolutePath() + "%" );
+    query.bindValue( ":prefix", path.canonicalPath() + "%" );
     query.exec();
 
     while( query.next() )
