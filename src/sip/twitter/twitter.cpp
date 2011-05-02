58692f424dad37a49034f19c9cd86c517bb67b62
Jeff Mitchell <tomahawk@jefferai.org>
2011-05-02 16:27:34 -0400

    Fix condition where duplicate connections could happen from twitter

--- src/sip/twitter/twitter.cpp
+++ src/sip/twitter/twitter.cpp
@@ -270,6 +270,7 @@ TwitterPlugin::connectTimerFired()
         {
             peerData["lastseen"] = QDateTime::currentMSecsSinceEpoch();
             m_cachedPeers[screenName] = peerData;
+            continue;
         }
         
         if ( QDateTime::currentMSecsSinceEpoch() - peerData["lastseen"].toLongLong() > 1209600000 ) // 2 weeks
