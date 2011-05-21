2590ae4ece8e840128f2c2213254bb48031f7bc4
Dominik Schmidt <dev@dominik-schmidt.de>
2011-05-21 04:38:55 +0200

    Make siptwitter optional and rely on system qtweetlib on default

--- src/sip/twitter/tomahawkoauthtwitter.h
+++ src/sip/twitter/tomahawkoauthtwitter.h
@@ -1,22 +1,23 @@
 #ifndef TOMAHAWKOAUTHTWITTER
 #define TOMAHAWKOAUTHTWITTER
 
+#include "../sipdllmacro.h"
+
+#include <qtweetlib_global.h>
 #include <oauthtwitter.h>
 
-#include "qtweetlib_global.h"
-
-class QTWEETLIBSHARED_EXPORT TomahawkOAuthTwitter : public OAuthTwitter
+class SIPDLLEXPORT TomahawkOAuthTwitter : public OAuthTwitter
 {
     Q_OBJECT
-    
+
 public:
     TomahawkOAuthTwitter( QNetworkAccessManager *nam, QObject *parent = 0 );
 
     ~TomahawkOAuthTwitter() {}
-    
+
 protected:
     virtual int authorizationWidget();
-    
+
 private slots:
     void error();
 };
