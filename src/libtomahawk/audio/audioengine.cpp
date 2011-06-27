75ec478caed605973db3f3d7720948e63a881ef6
Dominik Schmidt <dev@dominik-schmidt.de>
2011-06-27 21:06:36 +0200

    Fix streaming from https-links

--- src/libtomahawk/audio/audioengine.cpp
+++ src/libtomahawk/audio/audioengine.cpp
@@ -520,7 +520,7 @@ AudioEngine::setCurrentTrack( const Tomahawk::result_ptr& result )
 bool
 AudioEngine::isHttpResult( const QString& url ) const
 {
-    return url.startsWith( "http://" );
+    return url.startsWith( "http://" ) || url.startsWith( "https://" );
 }
 
 
