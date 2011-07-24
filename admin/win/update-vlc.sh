499f45402fdde9b7eac93f77db37b1dfd8c3ca98
Dominik Schmidt <dev@dominik-schmidt.de>
2011-07-25 00:31:12 +0200

    Build installers with phonon-vlc and vlc from 0.1 time because currently mingw32-phonon-vlc is broken

--- admin/win/update-vlc.sh
+++ admin/win/update-vlc.sh
@@ -5,17 +5,18 @@ rm -rf vlc/
 
 echo "Download specified binary..."
 #wget -c "http://downloads.sourceforge.net/project/vlc/1.1.9/win32/vlc-1.1.9-win32.7z?r=http%3A%2F%2Fwww.videolan.org%2Fvlc%2Fdownload-windows.html&ts=1306272584&use_mirror=leaseweb"
-wget -c "http://nightlies.videolan.org/build/win32/trunk-20110723-0002/vlc-1.2.0-git-20110723-0002-win32.7z"
+wget -c "http://download.tomahawk-player.org/tomahawk-vlc-0.1.zip"
 
 echo "Extract binary..."
-7z x vlc-*.7z
-mv -v vlc-*/ vlc/
+#7z x vlc-*.7z
+#mv -v vlc-*/ vlc/
+unzip tomahawk-vlc-0.1.zip
 
-echo "Strip unneeded plugins from vlc/plugins..."
-cd vlc/plugins/
-rm -rvf video_*/ gui/ */libold* */libvcd* */libdvd* */liblibass* */libx264* */libschroe* */liblibmpeg2* \
-     */libstream_out_* */libmjpeg_plugin* */libh264_plugin* */libzvbi_plugin* */lib*sub* \
-     services_discover/ visualization/ control/ meta_engine/ misc/
+#echo "Strip unneeded plugins from vlc/plugins..."
+# cd vlc/plugins/
+# rm -rvf video_*/ gui/ */libold* */libvcd* */libdvd* */liblibass* */libx264* */libschroe* */liblibmpeg2* \
+#      */libstream_out_* */libmjpeg_plugin* */libh264_plugin* */libzvbi_plugin* */lib*sub* \
+#      services_discover/ visualization/ control/ misc/
 
 
 echo "Downloaded and stripped VLC"
