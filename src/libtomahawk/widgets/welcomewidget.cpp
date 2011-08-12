b43fe66409f9793d4a676a4b05bcbff01d60b771
Leo Franchi <lfranchi@kde.org>
2011-08-12 15:34:21 -0400

    Hide header for track list in welcome widget

--- src/libtomahawk/widgets/welcomewidget.cpp
+++ src/libtomahawk/widgets/welcomewidget.cpp
@@ -81,6 +81,7 @@ WelcomeWidget::WelcomeWidget( QWidget* parent )
     m_tracksModel->setStyle( TrackModel::ShortWithAvatars );
     ui->tracksView->overlay()->setEnabled( false );
     ui->tracksView->setPlaylistModel( m_tracksModel );
+    ui->tracksView->setHeaderHidden( true );
     ui->tracksView->setHorizontalScrollBarPolicy( Qt::ScrollBarAlwaysOff );
 
     m_recentAlbumsModel = new AlbumModel( ui->additionsView );
