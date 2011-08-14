aff34a9dc52a7b9d77327ec2a0c2d5af9d74339e
Christian Muehlhaeuser <muesli@gmail.com>
2011-08-14 06:19:25 +0200

    * Removed debugging utility.

--- src/libtomahawk/utils/logger.cpp
+++ src/libtomahawk/utils/logger.cpp
@@ -85,9 +85,6 @@ TomahawkLogHandler( QtMsgType type, const char *msg )
 {
     static QMutex s_mutex;
 
-    if ( strstr( msg, "Pixmap" ) )
-        Q_ASSERT( false );
-
     QMutexLocker locker( &s_mutex );
     switch( type )
     {
