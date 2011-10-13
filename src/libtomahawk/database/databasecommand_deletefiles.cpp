cfed5be1ca6f6ee8a23a30c331fa5c1415a8ebf2
Jeff Mitchell <tomahawk@jefferai.org>
2011-10-13 10:43:42 -0400

    Let's try this again

--- src/libtomahawk/database/databasecommand_deletefiles.cpp
+++ src/libtomahawk/database/databasecommand_deletefiles.cpp
@@ -74,7 +74,7 @@ DatabaseCommand_DeleteFiles::exec( DatabaseImpl* dbi )
             tDebug() << "Deleting" << m_dir.path() << "from db for localsource" << srcid;
             TomahawkSqlQuery dirquery = dbi->newquery();
             QString path( "file://" + m_dir.canonicalPath() + "/%" );
-            dirquery.prepare( QString( "SELECT id, url FROM file WHERE source IS NULL AND url LIKE \"%1\"" ).arg( TomahawkUtils::sqlEscape( path ) ) );
+            dirquery.prepare( QString( "SELECT id, url FROM file WHERE source IS NULL AND url LIKE '%1'" ).arg( TomahawkUtils::sqlEscape( path ) ) );
             dirquery.exec();
 
             while ( dirquery.next() )
