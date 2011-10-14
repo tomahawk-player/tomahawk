ac7aadf615f4b6f686f5237d93eded235c44babb
Leo Franchi <lfranchi@kde.org>
2011-10-13 22:33:32 -0400

    Fix latching when switching directly between latches

--- src/libtomahawk/LatchManager.cpp
+++ src/libtomahawk/LatchManager.cpp
@@ -97,6 +97,17 @@ LatchManager::playlistChanged( PlaylistInterface* )
     cmd->setTimestamp( QDateTime::currentDateTime().toTime_t() );
     Database::instance()->enqueue( QSharedPointer< DatabaseCommand >( cmd ) );
 
+    if ( !m_waitingForLatch.isNull() &&
+          m_waitingForLatch != m_latchedOnTo )
+    {
+        // We are asked to latch on immediately to another source
+        m_latchedOnTo.clear();
+        m_latchedInterface.clear();
+
+        // call ourselves to hit the "create latch" condition
+        playlistChanged( 0 );
+        return;
+    }
     m_latchedOnTo.clear();
     m_waitingForLatch.clear();
     m_latchedInterface.clear();
