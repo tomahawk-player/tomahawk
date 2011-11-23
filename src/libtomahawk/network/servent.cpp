04deaba7884a21cc8489b085c57463691c76b3f0
Christian Muehlhaeuser <muesli@gmail.com>
2011-11-23 04:18:30 +0100

    * Less furious logging.

--- src/libtomahawk/network/servent.cpp
+++ src/libtomahawk/network/servent.cpp
@@ -351,7 +351,7 @@ Servent::readyRead()
 
         foreach( ControlConnection* con, m_controlconnections )
         {
-            tDebug() << "known connection:" << con->id() << con->source()->friendlyName();
+            tLog( LOGVERBOSE ) << "known connection:" << con->id() << con->source()->friendlyName();
             if( con->id() == nodeid )
             {
                 dupe = true;
