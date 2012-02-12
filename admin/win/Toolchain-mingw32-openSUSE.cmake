99143f6148cff96cc499ea83d94dc5c5d75b4367
Dominik Schmidt <dev@dominik-schmidt.de>
2012-02-12 20:17:35 +0100

    Fix windows for qt 4.8

--- admin/win/Toolchain-mingw32-openSUSE.cmake
+++ admin/win/Toolchain-mingw32-openSUSE.cmake
@@ -3,9 +3,12 @@ SET(MINGW_PREFIX  "i686-w64-mingw32")
 # this one is important
 SET(CMAKE_SYSTEM_NAME Windows)
 
+
 # specify the cross compiler
 SET(CMAKE_C_COMPILER ccache ${MINGW_PREFIX}-gcc)
+SET(CMAKE_C_FLAGS "-fno-keep-inline-dllexport")
 SET(CMAKE_CXX_COMPILER ccache ${MINGW_PREFIX}-g++)
+SET(CMAKE_CXX_FLAGS ${CMAKE_C_FLAGS})
 SET(CMAKE_RC_COMPILER /usr/bin/${MINGW_PREFIX}-windres)
 
 # where is the target environment containing libraries
@@ -15,9 +18,6 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE  ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM  NEVER)
 
 
-
-# libs with broken find modules
+# configure qt variables
+SET(QT_LIBRARY_DIR  /usr/${MINGW_PREFIX}/bin)
 SET(QT_PLUGINS_DIR  ${CMAKE_FIND_ROOT_PATH}/lib/qt4/plugins/)
-SET(QT_QTUITOOLS_LIBRARY_RELEASE  ${CMAKE_FIND_ROOT_PATH}/lib/libQtUiTools.a)
-SET(QT_QTUITOOLS_LIBRARY_DEBUG  ${CMAKE_FIND_ROOT_PATH}/lib/libQtUiToolsd.a)
-SET(QT_QTUITOOLS_LIBRARY  ${QT_QTUITOOLS_LIBRARY_RELEASE})
