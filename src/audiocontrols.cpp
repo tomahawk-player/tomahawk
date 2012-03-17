d86124203c8459dba5640927def285663f647aa7
Christian Muehlhaeuser <muesli@gmail.com>
2012-03-17 01:32:15 +0100

    * Don't crash in onCoverUpdated if there's no current track.

--- src/audiocontrols.cpp
+++ src/audiocontrols.cpp
@@ -252,7 +252,7 @@ void
 AudioControls::onCoverUpdated()
 {
     Query* query = qobject_cast< Query* >( sender() );
-    if ( !query || query != m_currentTrack->toQuery().data() )
+    if ( !query || !m_currentTrack || query != m_currentTrack->toQuery().data() )
         return;
 
     setCover();
