a3d11225a31038691223df43eaaf00516c8fc245
Christian Muehlhaeuser <muesli@gmail.com>
2012-05-23 09:38:26 +0200

    * Fixed compiling on OS X 10.6.

--- src/libtomahawk/utils/TomahawkUtils_Mac.mm
+++ src/libtomahawk/utils/TomahawkUtils_Mac.mm
@@ -16,16 +16,16 @@
  *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "TomahawkUtils.h"
-
-#include "TomahawkUtils_Mac.h"
 #include "mac/FileHelpers.h"
-
-#include <QDir>
-#include <QTemporaryFile>
 
 #import <AppKit/NSApplication.h>
 #import <Foundation/Foundation.h>
+
+#include "TomahawkUtils.h"
+#include "TomahawkUtils_Mac.h"
+
+#include <QDir>
+#include <QTemporaryFile>
 
 @implementation MoveDelegate
 
