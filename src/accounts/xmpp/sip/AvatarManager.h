c258f1897f674c90ad61620d6e920e6c7cecdb5c
Christian Muehlhaeuser <muesli@gmail.com>
2012-05-28 13:05:37 +0200

    * Style cleanup for AvatarManager.

--- src/accounts/xmpp/sip/AvatarManager.h
+++ src/accounts/xmpp/sip/AvatarManager.h
@@ -32,32 +32,32 @@ class ACCOUNTDLLEXPORT AvatarManager : public QObject
 Q_OBJECT
 
 public:
-    AvatarManager(Jreen::Client *client);
+    AvatarManager(Jreen::Client* client);
     virtual ~AvatarManager();
 
-    QPixmap avatar(const QString &jid) const;
+    QPixmap avatar(const QString& jid) const;
 
 signals:
-    void newAvatar( const QString &jid );
+    void newAvatar( const QString& jid );
 
 private slots:
     void onNewPresence( const Jreen::Presence& presence );
-    void onNewIq(const Jreen::IQ &iq);
+    void onNewIq(const Jreen::IQ& iq);
     void onNewConnection();
-    void onNewAvatar( const QString &jid );
+    void onNewAvatar( const QString& jid );
 
 private:
-    void fetchVCard( const QString &jid);
-    QString avatarHash(const QString &jid) const;
-    QString avatarPath(const QString &avatarHash) const;
+    void fetchVCard( const QString& jid );
+    QString avatarHash( const QString& jid ) const;
+    QString avatarPath( const QString& avatarHash ) const;
 
-    QDir avatarDir(const QString &avatarHash) const;
-    bool isCached(const QString &avatarHash) const;
+    QDir avatarDir( const QString& avatarHash ) const;
+    bool isCached( const QString& avatarHash ) const;
 
-    Jreen::Client *m_client;
+    Jreen::Client* m_client;
     QStringList m_cachedAvatars;
     QDir m_cacheDir;
-    QMap<QString, QString> m_JidsAvatarHashes;
+    QMap< QString, QString > m_JidsAvatarHashes;
 };
 
 #endif // AVATARMANAGER_H
