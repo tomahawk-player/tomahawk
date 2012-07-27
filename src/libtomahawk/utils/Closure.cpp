9d6b5e60fc57de530e4c0cf2d1d7ebcf29ab6632
Leo Franchi <lfranchi@kde.org>
2012-07-26 22:05:28 -0400

    Allow Closures to be created when running in a different thread from the receiver

--- src/libtomahawk/utils/Closure.cpp
+++ src/libtomahawk/utils/Closure.cpp
@@ -17,8 +17,14 @@
 */
 
 #include "Closure.h"
+#include <QApplication>
 
 namespace _detail {
+
+static bool on_this_thread(QObject* obj)
+{
+    return QCoreApplication::instance()->thread() == obj->thread();
+}
 
 Closure::Closure(QObject* sender,
                  const char* signal,
@@ -28,9 +34,10 @@ Closure::Closure(QObject* sender,
                  const ClosureArgumentWrapper* val1,
                  const ClosureArgumentWrapper* val2,
                  const ClosureArgumentWrapper* val3)
-    : QObject(receiver),
+    : QObject(on_this_thread(receiver) ? receiver : 0),
       callback_(NULL),
       autoDelete_(true),
+      outOfThreadReceiver_(on_this_thread(receiver) ? 0 : receiver ),
       val0_(val0),
       val1_(val1),
       val2_(val2),
@@ -78,7 +85,7 @@ void Closure::Invoked() {
     callback_();
   } else {
     slot_.invoke(
-        parent(),
+        parent() ? parent() : outOfThreadReceiver_,
         val0_ ? val0_->arg() : QGenericArgument(),
         val1_ ? val1_->arg() : QGenericArgument(),
         val2_ ? val2_->arg() : QGenericArgument(),
