db2374976da7286bba3410e17878f431784a485e
Teo Mrnjavac <teo@kde.org>
2012-09-15 00:39:30 +0300

    Fix typo in copyright header.

--- src/libtomahawk/Playlist.h
+++ src/libtomahawk/Playlist.h
@@ -1,6 +1,6 @@
 /* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
  *
- *   Copyright 2011920-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.org>
  *   Copyright 2010-2011, Leo Franchi <lfranchi@kde.org>
  *   Copyright 2010-2012, Jeff Mitchell <jeff@tomahawk-player.org>
  *
