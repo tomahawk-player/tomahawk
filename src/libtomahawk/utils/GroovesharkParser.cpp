ce713a416cf9825151d7a531f6286bc8d4447d2e
Christian Muehlhaeuser <muesli@gmail.com>
2012-11-16 11:45:15 +0100

    * Style fix.

--- src/libtomahawk/utils/GroovesharkParser.cpp
+++ src/libtomahawk/utils/GroovesharkParser.cpp
@@ -1,7 +1,7 @@
 /* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
  *
  *   Copyright 2010-2011, Leo Franchi <lfranchi@kde.org>
- *   Copyright 2010-2011, Hugo LindstrÃ¶m <hugolm84@gmail.com>
+ *   Copyright 2010-2011, Hugo Lindström <hugolm84@gmail.com>
  *   Copyright 2010-2012, Stefan Derkits <stefan@derkits.at>
  *
  *   Tomahawk is free software: you can redistribute it and/or modify
