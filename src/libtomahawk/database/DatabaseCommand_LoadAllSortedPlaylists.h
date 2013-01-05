8a373681b2d714193effbce5ad90b2ee69b08530
Dominik Schmidt <dev@dominik-schmidt.de>
2013-01-05 23:22:03 +0100

    qt5: ifdef two Q_DECLARE_METATYPE invocations out, for some reason they fail with Qt5

--- src/libtomahawk/database/DatabaseCommand_LoadAllSortedPlaylists.h
+++ src/libtomahawk/database/DatabaseCommand_LoadAllSortedPlaylists.h
@@ -61,5 +61,9 @@ private:
     DatabaseCommand_LoadAllPlaylists::SortAscDesc m_sortAscDesc;
 };
 
+//FIXME: Qt5: this fails with Qt5, is it needed at all? It compiles fine without in Qt4 as well
+#if ! QT_VERSION >= QT_VERSION_CHECK( 5, 0, 0 )
 Q_DECLARE_METATYPE(QList<DatabaseCommand_LoadAllSortedPlaylists::SourcePlaylistPair>)
+#endif
+
 #endif // DATABASECOMMAND_LOADALLSORTEDPLAYLISTS_H
--- src/libtomahawk/database/DatabaseCommand_LoadSocialActions.h
+++ src/libtomahawk/database/DatabaseCommand_LoadSocialActions.h
@@ -142,6 +142,9 @@ private:
 
 };
 
+//FIXME: Qt5: this fails with Qt5, is it needed at all? It compiles fine without in Qt4 as well
+#if ! QT_VERSION >= QT_VERSION_CHECK( 5, 0, 0 )
 Q_DECLARE_METATYPE( DatabaseCommand_LoadSocialActions::TrackActions )
+#endif
 
 #endif // DATABASECOMMAND_LOADSOCIALACTIONS_H
