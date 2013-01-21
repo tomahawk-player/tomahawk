4601d9b2136aa7c83133ef3ff362b7a9e00337b8
Christian Muehlhaeuser <muesli@gmail.com>
2013-01-21 15:45:48 +0100

    * Remove code which seems to cause painting issues with some styles.

--- src/sourcetree/SourceDelegate.cpp
+++ src/sourcetree/SourceDelegate.cpp
@@ -302,9 +302,6 @@ SourceDelegate::paintCategory( QPainter* painter, const QStyleOptionViewItem& op
 
     QTextOption to( Qt::AlignVCenter );
 
-    painter->setPen( option.palette.color( QPalette::Base ) );
-    painter->setBrush( option.palette.color( QPalette::Base ) );
-    painter->drawRect( option.rect );
     painter->setRenderHint( QPainter::Antialiasing );
 
     painter->setPen( Qt::white );
@@ -346,9 +343,6 @@ SourceDelegate::paintGroup( QPainter* painter, const QStyleOptionViewItem& optio
 
     QTextOption to( Qt::AlignBottom );
 
-    painter->setPen( option.palette.color( QPalette::Base ) );
-    painter->setBrush( option.palette.color( QPalette::Base ) );
-    painter->drawRect( option.rect );
     painter->setRenderHint( QPainter::Antialiasing );
 
     painter->setPen( Qt::white );
@@ -412,7 +406,6 @@ SourceDelegate::paint( QPainter* painter, const QStyleOptionViewItem& option, co
             else
                 o3.state &= ~QStyle::State_Selected;
 
-            o.palette.setColor( QPalette::Base, QColor( 0, 0, 0, 0 ) );
             o.palette.setColor( QPalette::Text, o.palette.color( QPalette::HighlightedText ) );
             o3.palette.setColor( QPalette::Text, o.palette.color( QPalette::HighlightedText ) );
         }
