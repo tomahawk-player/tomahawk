bd6303a6e6998dd781d1d6e092e9ac7e91ea9628
Hugo Lindström <hugolm84@gmail.com>
2013-04-01 21:12:18 +0200

    Bump version

--- src/infoplugins/generic/charts/ChartsPlugin.cpp
+++ src/infoplugins/generic/charts/ChartsPlugin.cpp
@@ -55,7 +55,7 @@ ChartsPlugin::ChartsPlugin()
     tDebug( LOGVERBOSE ) << Q_FUNC_INFO << QThread::currentThread();
 
     // If you add resource, update version aswell
-    m_chartVersion = "2.6.1";
+    m_chartVersion = "2.6.2";
     m_supportedGetTypes <<  InfoChart << InfoChartCapabilities;
 
     // Charts that have geo or genre types
