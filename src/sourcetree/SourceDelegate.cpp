552a830284cb184cbb1632ba3ee1113e619a7cd4
Christian Muehlhaeuser <muesli@gmail.com>
2013-04-16 10:59:23 +0200

    * Fixed foreground color.

--- src/sourcetree/SourceDelegate.cpp
+++ src/sourcetree/SourceDelegate.cpp
@@ -235,7 +235,7 @@ SourceDelegate::paintCollection( QPainter* painter, const QStyleOptionViewItem& 
         painter->drawText( textRect, text, to );
     }
 
-    QColor descColor = option.palette.color( QPalette::HighlightedText ).lighter( 220 );
+    QColor descColor = option.palette.color( QPalette::Text ).lighter( 220 );
     if ( type == SourcesModel::ScriptCollection && //you cannot select a non-script collection anyway
          option.state.testFlag( QStyle::State_Selected ) )
     {
