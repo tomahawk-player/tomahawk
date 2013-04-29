2537daa0b612938dcc6a7c710433715834385a72
Christian Muehlhaeuser <muesli@gmail.com>
2013-04-29 17:39:25 +0200

    * Moved TomahawkUtils::Colors into TomahawkColors and a separate file.

---
+++ src/libtomahawk/utils/TomahawkColors.h
@@ -0,0 +1,38 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2013, Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TOMAHAWKCOLORS_H
+#define TOMAHAWKCOLORS_H
+
+#include "TomahawkUtils.h"
+#include "DllMacro.h"
+
+#include <QColor>
+
+namespace TomahawkColors
+{
+    static const QColor BORDER_LINE = QColor( "#8c8c8c" );
+    static const QColor POPUP_BACKGROUND = QColor( "#ffffff" );
+    static const QColor GROUP_HEADER = QColor( "#637180" );
+    static const QColor NOW_PLAYING_ITEM = QColor( "#962c26" );
+    static const QColor NOW_PLAYING_ITEM_TEXT = QColor( "#ffffff" );
+    static const QColor SELECTION_BACKGROUND = QColor( "#962c26" );
+    static const QColor SELECTION_FOREGROUND = QColor( "#ffffff" );
+}
+
+#endif // TOMAHAWKCOLORS_H
--- src/libtomahawk/utils/TomahawkUtilsGui.h
+++ src/libtomahawk/utils/TomahawkUtilsGui.h
@@ -77,17 +77,6 @@ namespace TomahawkUtils
 
     DLLEXPORT void drawCompositedPopup( QWidget* widget, const QPainterPath& outline, const QColor& lineColor, const QBrush& backgroundBrush, qreal opacity );
 
-    namespace Colors
-    {
-        static const QColor BORDER_LINE = QColor( "#8c8c8c" );
-        static const QColor POPUP_BACKGROUND = QColor( "#ffffff" );
-        static const QColor GROUP_HEADER = QColor( "#637180" );
-        static const QColor NOW_PLAYING_ITEM = QColor( "#962c26" );
-        static const QColor NOW_PLAYING_ITEM_TEXT = QColor( "#ffffff" );
-        static const QColor SELECTION_BACKGROUND = QColor( "#962c26" );
-        static const QColor SELECTION_FOREGROUND = QColor( "#ffffff" );
-    }
-
     static const int POPUP_ROUNDING_RADIUS = 6;
     static const float POPUP_OPACITY = 0.96;
 }
