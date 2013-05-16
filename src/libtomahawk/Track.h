84c219105ed63a3c8f0c2f4f553a16b5afeebd0d
Christian Muehlhaeuser <muesli@gmail.com>
2013-05-16 11:49:17 +0200

    * Track is now proxying a TrackData object (which is cached and only loaded once per track-id).

--- src/libtomahawk/Track.h
+++ src/libtomahawk/Track.h
@@ -24,39 +24,18 @@
 #include <QVariant>
 
 #include "Typedefs.h"
+#include "TrackData.h"
 #include "infosystem/InfoSystem.h"
 
 #include "DllMacro.h"
 
-class DatabaseCommand_LogPlayback;
-class DatabaseCommand_PlaybackHistory;
-
 namespace Tomahawk
 {
-
-struct SocialAction
-{
-    QVariant action;
-    QVariant value;
-    QVariant timestamp;
-    Tomahawk::source_ptr source;
-};
-
-struct PlaybackLog
-{
-    Tomahawk::source_ptr source;
-    unsigned int timestamp;
-    unsigned int secsPlayed;
-};
-
-class TrackPrivate;
 
 class DLLEXPORT Track : public QObject
 {
 Q_OBJECT
 
-friend class ::DatabaseCommand_LogPlayback;
-friend class ::DatabaseCommand_PlaybackHistory;
 friend class Pipeline;
 
 public:
@@ -64,14 +43,13 @@ public:
     { Detailed = 0, Short = 1 };
 
     static track_ptr get( const QString& artist, const QString& track, const QString& album = QString(), int duration = 0, const QString& composer = QString(), unsigned int albumpos = 0, unsigned int discnumber = 0 );
+    static track_ptr get( unsigned int id, const QString& artist, const QString& track, const QString& album, int duration, const QString& composer, unsigned int albumpos, unsigned int discnumber );
 
     virtual ~Track();
 
-    QString id() const;
-
-    void setArtist( const QString& artist ) { m_artist = artist; updateSortNames(); }
+//    void setArtist( const QString& artist ) { m_artist = artist; updateSortNames(); }
     void setAlbum( const QString& album ) { m_album = album; updateSortNames(); }
-    void setTrack( const QString& track ) { m_track = track; updateSortNames(); }
+//    void setTrack( const QString& track ) { m_track = track; updateSortNames(); }
 /*    void setDuration( int duration ) { m_duration = duration; }
     void setAlbumPos( unsigned int albumpos ) { m_albumpos = albumpos; }
     void setDiscNumber( unsigned int discnumber ) { m_discnumber = discnumber; }
@@ -88,11 +66,12 @@ public:
     QString albumSortname() const { return m_albumSortname; }
     QString trackSortname() const { return m_trackSortname; }
 
-    QString artist() const { return m_artist; }
+    QString artist() const;
+    QString track() const;
     QString composer() const { return m_composer; }
     QString album() const { return m_album; }
-    QString track() const { return m_track; }
     int duration() const { return m_duration; }
+    int year() const;
     unsigned int albumpos() const { return m_albumpos; }
     unsigned int discnumber() const { return m_discnumber; }
 
@@ -105,21 +84,26 @@ public:
 #endif
     bool coverLoaded() const;
 
-    void setLoved( bool loved );
+    void setLoved( bool loved, bool postToInfoSystem = true );
     bool loved();
+
+    void share( const Tomahawk::source_ptr& source );
+
+    void loadAttributes();
+    QVariantMap attributes() const;
 
     void loadStats();
     QList< Tomahawk::PlaybackLog > playbackHistory( const Tomahawk::source_ptr& source = Tomahawk::source_ptr() ) const;
-    void setPlaybackHistory( const QList< Tomahawk::PlaybackLog >& playbackData );
     unsigned int playbackCount( const Tomahawk::source_ptr& source = Tomahawk::source_ptr() );
 
     void loadSocialActions();
     QList< Tomahawk::SocialAction > allSocialActions() const;
-    void setAllSocialActions( const QList< Tomahawk::SocialAction >& socialActions );
     QString socialActionDescription( const QString& action, DescriptionMode mode ) const;
 
     QList<Tomahawk::query_ptr> similarTracks() const;
     QStringList lyrics() const;
+
+    unsigned int trackId() const;
 
     QWeakPointer< Tomahawk::Track > weakRef() { return m_ownRef; }
     void setWeakRef( QWeakPointer< Tomahawk::Track > weakRef ) { m_ownRef = weakRef; }
@@ -127,6 +111,7 @@ public:
 signals:
     void coverChanged();
     void socialActionsLoaded();
+    void attributesLoaded();
     void statsLoaded();
     void similarTracksLoaded();
     void lyricsLoaded();
@@ -136,25 +121,21 @@ signals:
 public slots:
     void deleteLater();
 
-private slots:
-    void infoSystemInfo( Tomahawk::InfoSystem::InfoRequestData requestData, QVariant output );
-    void infoSystemFinished( QString target );
-
 private:
+    explicit Track( unsigned int id, const QString& artist, const QString& track, const QString& album, int duration, const QString& composer, unsigned int albumpos, unsigned int discnumber );
     explicit Track( const QString& artist, const QString& track, const QString& album, int duration, const QString& composer, unsigned int albumpos, unsigned int discnumber );
 
+    void init();
+
     void updateSortNames();
-    void parseSocialActions();
 
     QString m_artistSortname;
     QString m_composerSortname;
     QString m_albumSortname;
     QString m_trackSortname;
 
-    QString m_artist;
     QString m_composer;
     QString m_album;
-    QString m_track;
 
     int m_duration;
     unsigned int m_albumpos;
@@ -164,29 +145,16 @@ private:
     mutable Tomahawk::album_ptr m_albumPtr;
     mutable Tomahawk::artist_ptr m_composerPtr;
 
-    bool m_playbackHistoryLoaded;
-    QList< PlaybackLog > m_playbackHistory;
-
-    bool m_socialActionsLoaded;
-    QHash< QString, QVariant > m_currentSocialActions;
-    QList< SocialAction > m_allSocialActions;
-
-    bool m_simTracksLoaded;
-    QList<Tomahawk::query_ptr> m_similarTracks;
-
-    bool m_lyricsLoaded;
-    QStringList m_lyrics;
-
-    mutable int m_infoJobs;
-    mutable QString m_uuid;
+    mutable trackdata_ptr m_trackData;
 
     query_wptr m_query;
     QWeakPointer< Tomahawk::Track > m_ownRef;
+
+    static QHash< QString, track_wptr > s_tracksByName;
 };
 
 }; //ns
 
-Q_DECLARE_METATYPE( QList<Tomahawk::PlaybackLog> );
 Q_DECLARE_METATYPE( Tomahawk::track_ptr );
 
 #endif // TRACK_H
