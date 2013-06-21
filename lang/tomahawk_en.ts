6db92f0890e50d32d5e93f0d57def19e086bd380
Tomahawk CI <ci@tomahawk-player.org>
2013-06-21 02:16:54 +0200

    Automatic merge of Transifex translations

--- lang/tomahawk_en.ts
+++ lang/tomahawk_en.ts
@@ -924,9 +924,9 @@ Password</translation>
 <context>
     <name>JSResolver</name>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="585"/>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="620"/>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="656"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="590"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="625"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="661"/>
         <source>Script Resolver Warning: API call %1 returned data synchronously.</source>
         <translation>Script Resolver Warning: API call %1 returned data synchronously.</translation>
     </message>
@@ -1711,22 +1711,22 @@ Password</translation>
 <context>
     <name>ScriptEngine</name>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1129"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1134"/>
         <source>Script Resolver Error: %1 %2 %3 %4</source>
         <translation>Script Resolver Error: %1 %2 %3 %4</translation>
     </message>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1148"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1153"/>
         <source>SSL Error</source>
         <translation>SSL Error</translation>
     </message>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1149"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1154"/>
         <source>You have asked Tomahawk to connect securely to &lt;b&gt;%1&lt;/b&gt;, but we can&apos;t confirm that your connection is secure:&lt;br&gt;&lt;br&gt;&lt;b&gt;%2&lt;/b&gt;&lt;br&gt;&lt;br&gt;Do you want to trust this connection?</source>
         <translation>You have asked Tomahawk to connect securely to &lt;b&gt;%1&lt;/b&gt;, but we can&apos;t confirm that your connection is secure:&lt;br&gt;&lt;br&gt;&lt;b&gt;%2&lt;/b&gt;&lt;br&gt;&lt;br&gt;Do you want to trust this connection?</translation>
     </message>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1156"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1161"/>
         <source>Trust certificate</source>
         <translation>Trust certificate</translation>
     </message>
--- lang/tomahawk_zh_CN.ts
+++ lang/tomahawk_zh_CN.ts
@@ -922,9 +922,9 @@ Password</source>
 <context>
     <name>JSResolver</name>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="585"/>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="620"/>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="656"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="590"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="625"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="661"/>
         <source>Script Resolver Warning: API call %1 returned data synchronously.</source>
         <translation type="unfinished"/>
     </message>
@@ -1709,22 +1709,22 @@ Password</source>
 <context>
     <name>ScriptEngine</name>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1129"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1134"/>
         <source>Script Resolver Error: %1 %2 %3 %4</source>
         <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1148"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1153"/>
         <source>SSL Error</source>
         <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1149"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1154"/>
         <source>You have asked Tomahawk to connect securely to &lt;b&gt;%1&lt;/b&gt;, but we can&apos;t confirm that your connection is secure:&lt;br&gt;&lt;br&gt;&lt;b&gt;%2&lt;/b&gt;&lt;br&gt;&lt;br&gt;Do you want to trust this connection?</source>
         <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1156"/>
+        <location filename="../src/libtomahawk/resolvers/JSResolver.cpp" line="1161"/>
         <source>Trust certificate</source>
         <translation type="unfinished"/>
     </message>
