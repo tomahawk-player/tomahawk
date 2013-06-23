e41e1ac1bb7019f3fb9113c7a0a51a527b664221
Christian Muehlhaeuser <muesli@gmail.com>
2013-06-23 22:40:28 +0200

    * Added xhochy to AUTHORS / about-dialog.

--- src/tomahawk/TomahawkWindow.cpp
+++ src/tomahawk/TomahawkWindow.cpp
@@ -1207,7 +1207,7 @@ TomahawkWindow::showAboutTomahawk()
     const QString thanksto( tr( "Thanks to:" ) );
 
     desc = QString( "%1<br/>Christian Muehlhaeuser &lt;muesli@tomahawk-player.org&gt;<br/><br/>"
-    "%2 Leo Franchi, Jeff Mitchell, Dominik Schmidt, Jason Herskowitz, Alejandro Wainzinger, Hugo Lindstr&ouml;m, Syd Lawrence, Michael Zanetti, Teo Mrnjavac, Christopher Reichert, Harald Sitter" )
+    "%2 Leo Franchi, Jeff Mitchell, Dominik Schmidt, Jason Herskowitz, Alejandro Wainzinger, Hugo Lindstr&ouml;m, Michael Zanetti, Teo Mrnjavac, Christopher Reichert, Uwe L. Korn, Harald Sitter, Syd Lawrence" )
               .arg( copyright )
               .arg( thanksto );
 
