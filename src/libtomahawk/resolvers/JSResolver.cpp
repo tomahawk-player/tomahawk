583053661fad4788097b5bd1f895d4c32e3b9e9e
Uwe L. Korn <uwelk@xhochy.com>
2013-06-26 14:41:19 +0200

    Revert "Only display filename in JS resolver errors/as scriptname"
    This reverts commit a6459f28237fe3fc3889e914f3d3c8c49d651d60.

--- src/libtomahawk/resolvers/JSResolver.cpp
+++ src/libtomahawk/resolvers/JSResolver.cpp
@@ -230,12 +230,12 @@ JSResolver::init()
         }
         const QByteArray reqContents = reqFile.readAll();
 
-        d->engine->setScriptPath( QFileInfo( s ).fileName() );
+        d->engine->setScriptPath( s );
         d->engine->mainFrame()->evaluateJavaScript( reqContents );
     }
 
     // add resolver
-    d->engine->setScriptPath( QFileInfo( filePath() ).fileName() );
+    d->engine->setScriptPath( filePath() );
     d->engine->mainFrame()->evaluateJavaScript( scriptContents );
 
     // init resolver
