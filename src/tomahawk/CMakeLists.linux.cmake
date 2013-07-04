833b6cbbf0349341d3c5aa973e58f92bc3558191
Uwe L. Korn <uwelk@xhochy.com>
2013-07-04 10:31:48 +0200

    There is no CMakeLists in linux/ anymore

--- src/tomahawk/CMakeLists.linux.cmake
+++ src/tomahawk/CMakeLists.linux.cmake
@@ -15,8 +15,6 @@ IF( WITH_GNOMESHORTCUTHANDLER )
     SET( tomahawkSources ${tomahawkSources} linux/GnomeShortcutHandler.cpp )
 ENDIF( WITH_GNOMESHORTCUTHANDLER )
 
-ADD_SUBDIRECTORY( "${CMAKE_CURRENT_SOURCE_DIR}/linux" )
-
 INSTALL( FILES ${CMAKE_SOURCE_DIR}/data/icons/tomahawk-icon.svg RENAME tomahawk.svg  DESTINATION ${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps )
 
 INSTALL( FILES ${CMAKE_SOURCE_DIR}/admin/unix/tomahawk.desktop DESTINATION ${CMAKE_INSTALL_DATADIR}/applications )
