5a6b2ad4adb5a48d1e434d7195ef8a6e8f2c8b54
Patrick von Reth <vonreth@kde.org>
2013-07-24 11:54:15 +0200

    relicenced find script

--- CMakeModules/FindLibsnore.cmake
+++ CMakeModules/FindLibsnore.cmake
@@ -1,25 +1,11 @@
-########################################################################################
-# Copyright (c) 2010 Patrick von Reth <patrick.vonreth@gmail.com>                      #
-#                                                                                      #
-# This program is free software; you can redistribute it and/or modify it under        #
-# the terms of the GNU General Public License as published by the Free Software        #
-# Foundation; either version 2 of the License, or (at your option) any later           #
-# version.                                                                             #
-#                                                                                      #
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY      #
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A      #
-# PARTICULAR PURPOSE. See the GNU General Public License for more details.             #
-#                                                                                      #
-# You should have received a copy of the GNU General Public License along with         #
-# this program.  If not, see <http://www.gnu.org/licenses/>.                           #
-########################################################################################
-
 # - Try to find the libsnore library
-# Once done this will define
+#  Once done this will define
 #
 #  LIBSNORE_FOUND - system has the LIBSNORE library
 #  LIBSNORE_LIBRARIES - The libraries needed to use LIBSNORE
+#  LIBSNORE_INCLUDE_DIRS - The includes needed to use LIBSNORE
 #  LIBSNORE_PLUGIN_PATH - Path of the plugins
+#  Copyright 2013 Patrick von Reth <vonreth@kde.org>
 
 find_path(LIBSNORE_INCLUDE_DIR
   NAMES snore/core/snore.h
