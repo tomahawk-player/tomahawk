5c6994ae857bdd26814a6431e08b8635c1e54c0b
Patrick von Reth <vonreth@kde.org>
2013-07-24 12:01:59 +0200

    as sugested by Dominik Schmidt remove the copiright header which was probably related to the dbus code

--- src/infoplugins/generic/snorenotify/SnoreNotifyPlugin.cpp
+++ src/infoplugins/generic/snorenotify/SnoreNotifyPlugin.cpp
@@ -18,26 +18,6 @@
  *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
  */
 
-// Marked portions of this file are subject to the following copyright:
-/*
- * Copyright (C) 2009 by Aur??lien G??teau <aurelien.gateau@canonical.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2, or (at your option)
- * any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- */
-
 #include "SnoreNotifyPlugin.h"
 
 
