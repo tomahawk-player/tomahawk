91b01038b1e1ecaa6f4c0fdd3d8b02a89e707189
Uwe L. Korn <uwelk@xhochy.com>
2013-07-30 12:58:04 +0200

    Fix DB migration

--- data/sql/dbmigrate-30_to_31.sql
+++ data/sql/dbmigrate-30_to_31.sql
@@ -5,3 +5,5 @@ CREATE INDEX playlist_item_trackname ON playlist_item(trackname);
 CREATE INDEX playlist_item_artistname ON playlist_item(artistname);
 CREATE INDEX artist_name ON artist(name);
 CREATE INDEX track_name ON track(name);
+
+UPDATE settings SET v = '31' WHERE k == 'schema_version';
