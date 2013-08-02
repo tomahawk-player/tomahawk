be2af20ed0e3b45892f9b7e29916f52a97ec28cb
Dominik Schmidt <dev@dominik-schmidt.de>
2013-08-02 13:23:10 +0200

    Add ViewPageLazyLoader template

---
+++ src/libtomahawk/ViewPageLazyLoader.h
@@ -0,0 +1,58 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright 2013,    Dominik Schmidt <domme@tomahawk-player.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef VIEWPAGELAZYLOADER_H
+#define VIEWPAGELAZYLOADER_H
+
+#include "ViewPagePlugin.h"
+
+#include "DllMacro.h"
+
+namespace Tomahawk
+{
+
+template <class T>
+class ViewPageLazyLoader : public ViewPagePlugin
+{
+public:
+    ViewPageLazyLoader<T>()
+        : m_widget( 0 )
+    {
+    }
+
+    virtual ~ViewPageLazyLoader()
+    {
+        delete m_widget;
+    }
+
+    virtual T* widget()
+    {
+        if( !m_widget )
+            m_widget = new T();
+
+        return m_widget;
+    }
+
+
+protected:
+    T* m_widget;
+};
+
+} // ns
+
+#endif //VIEWPAGELAZYLOADER_H
