bffd198e53014e0df2c1afb01db1fb3ce996c863
Christian Muehlhaeuser <muesli@gmail.com>
2013-09-03 12:42:22 +0200

    * Print out invalid json message when it occurs.

--- src/libtomahawk/network/DbSyncConnection.cpp
+++ src/libtomahawk/network/DbSyncConnection.cpp
@@ -191,7 +191,7 @@ DBSyncConnection::handleMsg( msg_ptr msg )
     QVariantMap m = msg->json().toMap();
     if ( m.empty() )
     {
-        tLog() << "Failed to parse msg in dbsync" << m_source->id() << m_source->friendlyName();
+        tLog() << "Failed to parse msg in dbsync from:" << m_source->id() << m_source->friendlyName() << msg->json();
         Q_ASSERT( false );
         return;
     }
