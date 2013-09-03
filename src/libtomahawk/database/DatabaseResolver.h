7e14c2113527385bdc5f1f4f5d6f3f6ba75a0d6a
Christian Muehlhaeuser <muesli@gmail.com>
2013-09-03 12:38:41 +0200

    * Removed obsolete preference from DatabaseResolver.

--- src/libtomahawk/database/DatabaseResolver.h
+++ src/libtomahawk/database/DatabaseResolver.h
@@ -35,7 +35,6 @@ public:
 
     virtual QString name() const;
     virtual unsigned int weight() const { return m_weight; }
-    virtual unsigned int preference() const { return 100; }
     virtual unsigned int timeout() const { return 0; }
 
 public slots:
