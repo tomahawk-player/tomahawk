1b6fc39344b33c825c9e80840ff6420256f39605
Jason Herskowitz <jherskowitz@globallistic.com>
2013-09-08 17:13:53 -0400

    Change Network Activity to Trending to make it more clear as to what it is

--- src/viewpages/networkactivity/NetworkActivity.h
+++ src/viewpages/networkactivity/NetworkActivity.h
@@ -56,7 +56,7 @@ public:
     virtual const QString defaultName() { return QLatin1String( "networkactivity" ); }
     virtual QString title() const { return tr( "Trending" ); }
     virtual QString description() const { return tr( "Listening activity among your friends" ); }
-    virtual const QString pixmapPath() const { return ( RESPATH "images/network-activity.svg" ); }
+    virtual const QString pixmapPath() const { return ( RESPATH "images/trending.svg" ); }
     virtual bool showInfoBar() const { return true; }
     
     virtual int sortValue() { return 2; }
