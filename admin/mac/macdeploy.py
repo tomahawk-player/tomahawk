265b76ada327abe93372ef3e25faaf61af7077d2
Christian Muehlhaeuser <muesli@gmail.com>
2014-07-21 18:20:48 +0200

    Include whatsnew_0_8.so.

--- admin/mac/macdeploy.py
+++ admin/mac/macdeploy.py
@@ -216,6 +216,7 @@ TOMAHAWK_PLUGINS = [
   'libtomahawk_viewpage_dashboard.so',
   'libtomahawk_viewpage_networkactivity.so',
   'libtomahawk_viewpage_whatshot.so',
+  'libtomahawk_viewpage_whatsnew_0_8.so',
 ]
 
 QT_PLUGINS_SEARCH_PATH=[
