8542ad188e16596721ed6bff8dbef664c86b6229
Christian Muehlhaeuser <muesli@gmail.com>
2014-08-21 07:33:53 +0200

    * Don't try to package viewpage_networkactivity on OSX for now.

--- admin/mac/macdeploy.py
+++ admin/mac/macdeploy.py
@@ -221,7 +221,7 @@ TOMAHAWK_PLUGINS = [
   'libtomahawk_infoplugin_snorenotify.so',
   'libtomahawk_infoplugin_spotify.so',
   'libtomahawk_viewpage_dashboard.so',
-  'libtomahawk_viewpage_networkactivity.so',
+#  'libtomahawk_viewpage_networkactivity.so',
   'libtomahawk_viewpage_whatshot.so',
   'libtomahawk_viewpage_whatsnew_0_8.so',
 ]
