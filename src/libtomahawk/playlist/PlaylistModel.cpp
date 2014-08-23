7bdb67c88a7b92fe8643d003bde95ea4bede830f
Christian Muehlhaeuser <muesli@gmail.com>
2014-08-23 13:38:40 +0200

    * Fixed PlaylistModel's header.

--- src/libtomahawk/playlist/PlaylistModel.cpp
+++ src/libtomahawk/playlist/PlaylistModel.cpp
@@ -1,7 +1,7 @@
 /* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
  *
- *   Copyright 2010-2011, Christian Muehlhaeuser <muesli@tomahawk-player.o
- *   Copyright 2010-2012, Jeff Mitchell <jeff@tomahawk-player.org>rg>
+ *   Copyright 2010-2014, Christian Muehlhaeuser <muesli@tomahawk-player.org>
+ *   Copyright 2010-2012, Jeff Mitchell <jeff@tomahawk-player.org>
  *   Copyright 2010-2012, Leo Franchi <lfranchi@kde.org>
  *
  *   Tomahawk is free software: you can redistribute it and/or modify
