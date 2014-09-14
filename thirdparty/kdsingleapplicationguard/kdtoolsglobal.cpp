6afbdaef25af2139adfd0efccee243fd01bd926c
Uwe L. Korn <uwelk@xhochy.com>
2014-09-14 20:16:41 +0100

    Add explicit narrowing cast for C++11 support
    This reflects the existing behaviour but in C++11 the implicit cast is
    no longer allowed.

--- thirdparty/kdsingleapplicationguard/kdtoolsglobal.cpp
+++ thirdparty/kdsingleapplicationguard/kdtoolsglobal.cpp
@@ -21,12 +21,12 @@ namespace {
 static Version kdParseQtVersion( const char * const version ) {
     if ( !version || qstrlen( version ) < 5 || version[1] != '.' || version[3] != '.' || ( version[5] != 0 && version[5] != '.' && version[5] != '-' ) )
 	return Version(); // parse error
-    const Version result = { { version[0] - '0', version[2] - '0', version[4] - '0' } };
+    const Version result = { { static_cast<unsigned char>(version[0] - '0'), static_cast<unsigned char>(version[2] - '0'), static_cast<unsigned char>(version[4] - '0') } };
     return result;
 }
 
 bool _kdCheckQtVersion_impl( int major, int minor, int patchlevel ) {
     static const Version actual = kdParseQtVersion( qVersion() ); // do this only once each run...
-    const Version requested = { { major, minor, patchlevel } };
+    const Version requested = { { static_cast<unsigned char>( major ), static_cast<unsigned char>( minor ), static_cast<unsigned char>( patchlevel ) } };
     return actual >= requested;
 }
