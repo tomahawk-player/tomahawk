7f93604dbed63871d8f649e96f5170e1680eaafb
Uwe L. Korn <uwelk@xhochy.com>
2014-10-22 10:53:54 +0200

    Remove redundant code

--- src/libtomahawk/audio/AudioEngine.cpp
+++ src/libtomahawk/audio/AudioEngine.cpp
@@ -193,7 +193,6 @@ AudioEngine::AudioEngine()
     connect( d->audioOutput, SIGNAL( tick( qint64 ) ), SLOT( timerTriggered( qint64 ) ) );
     connect( d->audioOutput, SIGNAL( aboutToFinish() ), SLOT( onAboutToFinish() ) );
 
-    onVolumeChanged( d->audioOutput->volume() );
     setVolume( TomahawkSettings::instance()->volume() );
 
     qRegisterMetaType< AudioErrorCode >("AudioErrorCode");
