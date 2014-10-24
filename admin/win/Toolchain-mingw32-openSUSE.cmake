427d559c965bbe93e363bde81a142585aec0f23b
Dominik Schmidt <dev@dominik-schmidt.de>
2014-10-24 17:49:35 +0200

    Fix windows toolchain file

--- admin/win/Toolchain-mingw32-openSUSE.cmake
+++ admin/win/Toolchain-mingw32-openSUSE.cmake
@@ -8,6 +8,8 @@ SET(CMAKE_SYSTEM_NAME Windows)
 SET(CMAKE_C_COMPILER ${MINGW_PREFIX}-gcc)
 SET(CMAKE_CXX_COMPILER ${MINGW_PREFIX}-g++)
 SET(CMAKE_RC_COMPILER ${MINGW_PREFIX}-windres)
+SET(CMAKE_AR /usr/bin/${MINGW_PREFIX}-ar CACHE FILEPATH "ar")
+
 
 # where is the target environment containing libraries
 SET(CMAKE_FIND_ROOT_PATH  /usr/${MINGW_PREFIX}/sys-root/mingw)
@@ -26,12 +28,11 @@ SET(QT_PLUGINS_DIR          ${CMAKE_FIND_ROOT_PATH}/lib/qt4/plugins)
 SET(QT_MKSPECS_DIR          ${CMAKE_FIND_ROOT_PATH}/share/qt4/mkspecs)
 SET(QT_QT_INCLUDE_DIR       ${CMAKE_FIND_ROOT_PATH}/include)
 
-# qt tools
-SET(QT_QMAKE_EXECUTABLE     ${MINGW_PREFIX}-qmake )
-SET(QT_MOC_EXECUTABLE       ${MINGW_PREFIX}-moc)
-SET(QT_RCC_EXECUTABLE       ${MINGW_PREFIX}-rcc)
-SET(QT_UIC_EXECUTABLE       ${MINGW_PREFIX}-uic)
-SET(QT_LRELEASE_EXECUTABLE  ${MINGW_PREFIX}-lrelease)
+SET(QT_QMAKE_EXECUTABLE     /usr/bin/${MINGW_PREFIX}-qmake CACHE FILEPATH "qmake" )
+SET(QT_MOC_EXECUTABLE       /usr/bin/${MINGW_PREFIX}-moc CACHE FILEPATH "moc")
+SET(QT_RCC_EXECUTABLE       /usr/bin/${MINGW_PREFIX}-rcc CACHE FILEPATH "rcc")
+SET(QT_UIC_EXECUTABLE       /usr/bin/${MINGW_PREFIX}-uic CACHE FILEPATH "uic")
+SET(QT_LRELEASE_EXECUTABLE  /usr/bin/${MINGW_PREFIX}-lrelease CACHE FILEPATH "lrelease")
 
 # phonon -_-
 SET(Automoc4_DIR	    "/usr/lib64/automoc4/")
