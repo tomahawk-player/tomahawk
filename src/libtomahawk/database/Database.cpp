0c37308c9c85966532b7f9c711ee774c0753ad58
Christian Muehlhaeuser <muesli@gmail.com>
2014-10-25 03:56:04 +0200

    Emit signal when shutdown gets blocked by outstanding db jobs.

--- src/libtomahawk/database/Database.cpp
+++ src/libtomahawk/database/Database.cpp
@@ -157,16 +157,17 @@ Database::~Database()
         workerThread->quit();
     }
 
+    emit waitingForWorkers();
     if ( m_workerRW )
     {
-        m_workerRW.data()->wait( 60000 );
+        m_workerRW.data()->wait();
         delete m_workerRW.data();
     }
     foreach ( QPointer< DatabaseWorkerThread > workerThread, m_workerThreads )
     {
         if ( workerThread )
         {
-            workerThread.data()->wait( 60000 );
+            workerThread.data()->wait();
             delete workerThread.data();
         }
     }
@@ -176,6 +177,7 @@ Database::~Database()
     qDeleteAll( m_commandFactories.values() );
     delete m_impl;
 
+    emit workersFinished();
 }
 
 
