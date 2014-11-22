f48d07c77f6cec3bae108ed59306d0f96453feec
Christian Muehlhaeuser <muesli@gmail.com>
2014-11-22 19:08:02 +0100

    Fixed include.

--- src/infoplugins/linux/fdonotify/FdoNotifyPlugin.cpp
+++ src/infoplugins/linux/fdonotify/FdoNotifyPlugin.cpp
@@ -49,7 +49,7 @@
 #include "utils/Logger.h"
 #include "utils/TomahawkUtilsGui.h"
 
-#include "config.h"
+#include "TomahawkVersion.h"
 
 #include <QDBusConnection>
 #include <QDBusPendingCallWatcher>
