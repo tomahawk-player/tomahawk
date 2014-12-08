6a239e24d15d2c0eefc12b7822b78697665a58f5
Dominik Schmidt <dev@dominik-schmidt.de>
2014-12-08 23:53:28 +0100

    Add ScriptPlugin class

---
+++ src/libtomahawk/resolvers/ScriptPlugin.h
@@ -0,0 +1,44 @@
+/* === This file is part of Tomahawk Player - <http://tomahawk-player.org> ===
+ *
+ *   Copyright (C) 2014  Dominik Schmidt <domme@tomahawk-player.org>
+ *
+ *   Tomahawk is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Tomahawk is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Tomahawk. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+#ifndef TOMAHAWK_SCRIPTPLUGIN_H
+#define TOMAHAWK_SCRIPTPLUGIN_H
+
+#include "../DllMacro.h"
+
+namespace Tomahawk {
+
+class ScriptObject;
+
+class DLLEXPORT ScriptPlugin
+{
+public:
+    ScriptPlugin( ScriptObject* object );
+    virtual ~ScriptPlugin();
+
+    ScriptObject* scriptObject() const;
+
+private: // TODO: pimple
+    ScriptObject* m_object;
+
+};
+
+} // ns: Tomahawk
+
+#endif // TOMAHAWK_SCRIPTPLUGIN_H
