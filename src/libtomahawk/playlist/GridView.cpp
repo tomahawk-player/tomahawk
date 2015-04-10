4cef333162205bff4af4c51fba185e7c84741fbe
Christian Muehlhaeuser <muesli@gmail.com>
2015-04-10 03:52:38 +0200

    Apply scrolling fix for GridViews on OS X, too.

--- src/libtomahawk/playlist/GridView.cpp
+++ src/libtomahawk/playlist/GridView.cpp
@@ -248,12 +248,10 @@ GridView::resizeEvent( QResizeEvent* event )
 
 void GridView::wheelEvent( QWheelEvent* e )
 {
-#ifndef Q_WS_MAC
     //HACK: Workaround for QTBUG-7232: Smooth scrolling (scroll per pixel) in ItemViews
     //      does not work as expected.
     verticalScrollBar()->setSingleStep( delegate()->itemSize().height() / 8 );
                                      // ^ scroll step is 1/8 of the estimated row height
-#endif
 
     QListView::wheelEvent( e );
 }
