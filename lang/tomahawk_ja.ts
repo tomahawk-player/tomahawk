f114ef4daea802b93f175f1802cf38495512e135
Tomahawk CI <ci@tomahawk-player.org>
2015-04-12 02:17:54 +0200

    Automatic merge of Transifex translations

--- lang/tomahawk_ja.ts
+++ lang/tomahawk_ja.ts
@@ -4058,7 +4058,7 @@ Try tweaking the filters for a new set of songs to play.</source>
     <message>
         <location filename="../src/tomahawk/TomahawkWindow.cpp" line="1428"/>
         <source>Copyright 2010 - 2015</source>
-        <translation type="unfinished"/>
+        <translation>Copyright 2010 - 2015 {2010 ?}</translation>
     </message>
     <message>
         <location filename="../src/tomahawk/TomahawkWindow.cpp" line="1429"/>
