46c9f84bb36372cea46d9f3e2033d88f7ba4af6a
Christian Muehlhaeuser <muesli@gmail.com>
2015-04-15 06:33:18 +0200

    Fixed source translation for copyright.

--- lang/tomahawk_en.ts
+++ lang/tomahawk_en.ts
@@ -4059,7 +4059,7 @@ Try tweaking the filters for a new set of songs to play.</translation>
     <message>
         <location filename="../src/tomahawk/TomahawkWindow.cpp" line="1428"/>
         <source>Copyright 2010 - 2015</source>
-        <translation>Copyright 2010 - 2015 {2010 ?}</translation>
+        <translation>Copyright 2010 - 2015</translation>
     </message>
     <message>
         <location filename="../src/tomahawk/TomahawkWindow.cpp" line="1429"/>
