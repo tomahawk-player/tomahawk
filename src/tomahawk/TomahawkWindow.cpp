566c655ac6e859fb0a22f7b2fef6f8fc3ec40433
Samuel Rey <savillavicencio@csumb.edu>
2015-10-17 19:21:36 -0700

    Added Ctrl-N and Ctrl-S shortcuts for new playlist and station, respectively.

--- src/tomahawk/TomahawkWindow.cpp
+++ src/tomahawk/TomahawkWindow.cpp
@@ -689,6 +689,8 @@ TomahawkWindow::setupSignals()
     connect( ac->getAction( "aboutTomahawk" ), SIGNAL( triggered() ), SLOT( showAboutTomahawk() ) );
     connect( ac->getAction( "quit" ), SIGNAL( triggered() ), qApp, SLOT( quit() ) );
     connect( ac->getAction( "showOfflineSources" ), SIGNAL( triggered() ), SLOT( showOfflineSources() ) );
+    connect( ac->getAction( "createPlaylist" ), SIGNAL( triggered() ), SLOT( createPlaylist() ) );
+    connect( ac->getAction( "createStation" ), SIGNAL( triggered() ), SLOT( createStation() ) );
 
 #if defined( Q_OS_MAC )
     connect( ac->getAction( "minimize" ), SIGNAL( triggered() ), SLOT( minimize() ) );
