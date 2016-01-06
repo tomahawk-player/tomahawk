d7e5ce6e4e7fc06546b41e8c0804d112b7b49b3c
Dominik Schmidt <domme@tomahawk-player.org>
2016-01-06 04:52:53 +0100

    Put crashreporter topLabel text into Tomahawk instead of libcrashreporter-qt

--- src/crashreporter/main.cpp
+++ src/crashreporter/main.cpp
@@ -174,7 +174,7 @@ int main( int argc, char* argv[] )
     reporter.setLogo( QPixmap( CRASHREPORTER_ICON ) );
     #endif
     reporter.setWindowTitle( CRASHREPORTER_PRODUCT_NAME );
-    reporter.setText("<html><head/><body><p><span style=\" font-weight:600;\">Sorry!</span> " CRASHREPORTER_PRODUCT_NAME " crashed. Please tell us about it! " CRASHREPORTER_PRODUCT_NAME " has created an error report for you that can help improve the stability in the future. You can now send this report directly to the " CRASHREPORTER_PRODUCT_NAME " developers.</p></body></html>");
+    reporter.setText("<html><head/><body><p><span style=\"font-weight:600;\">Sorry!</span> " CRASHREPORTER_PRODUCT_NAME " crashed. Please tell us about it! " CRASHREPORTER_PRODUCT_NAME " has created an error report for you that can help improve the stability in the future. You can now send this report directly to the " CRASHREPORTER_PRODUCT_NAME " developers.</p><p>Can you tell us what you were doing when this happened?</p></body></html>");
 
     reporter.setReportData( "BuildID", CRASHREPORTER_BUILD_ID );
     reporter.setReportData( "ProductName",  CRASHREPORTER_PRODUCT_NAME );
