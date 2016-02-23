f38f3ca367e74e30c82e6752f659963e2e7e5a90
Dominik Schmidt <domme@tomahawk-player.org>
2016-02-23 16:13:08 +0100

    Linux: Fix icon installation

--- src/tomahawk/CMakeLists.linux.cmake
+++ src/tomahawk/CMakeLists.linux.cmake
@@ -1,8 +1,8 @@
-FILE( GLOB _icons "${CMAKE_SOURCE_DIR}/data/icons/tomahawk-icon-*.png" )
+FILE( GLOB _icons "${CMAKE_SOURCE_DIR}/data/icons/*-tomahawk-icon.png" )
 FOREACH( _file ${_icons} )
- STRING( REPLACE "${CMAKE_SOURCE_DIR}/data/icons/tomahawk-icon-" "" _res ${_file} )
- STRING( REPLACE ".png" "" _res ${_res} )
- INSTALL( FILES ${_file} RENAME tomahawk.png DESTINATION ${CMAKE_INSTALL_DATADIR}/icons/hicolor/${_res}/apps )
+ STRING( REPLACE "${CMAKE_SOURCE_DIR}/data/icons/" "" _res ${_file} )
+ STRING( REPLACE "-tomahawk-icon.png" "" _res ${_res} )
+ INSTALL( FILES ${_file} RENAME ${TOMAHAWK_TARGET_NAME}.png DESTINATION ${CMAKE_INSTALL_DATADIR}/icons/hicolor/${_res}/apps )
 ENDFOREACH( _file )
 
 IF( WITH_UPOWER )
@@ -15,6 +15,7 @@ IF( WITH_GNOMESHORTCUTHANDLER )
     SET( tomahawkSources ${tomahawkSources} linux/GnomeShortcutHandler.cpp )
 ENDIF( WITH_GNOMESHORTCUTHANDLER )
 
-INSTALL( FILES ${CMAKE_SOURCE_DIR}/data/icons/tomahawk-icon.svg RENAME tomahawk.svg  DESTINATION ${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps )
+INSTALL( FILES ${CMAKE_SOURCE_DIR}/data/icons/tomahawk-icon.svg RENAME ${TOMAHAWK_TARGET_NAME}.svg  DESTINATION ${CMAKE_INSTALL_DATADIR}/icons/hicolor/scalable/apps )
 
-INSTALL( FILES ${CMAKE_SOURCE_DIR}/admin/unix/tomahawk.desktop DESTINATION ${CMAKE_INSTALL_DATADIR}/applications )
+INSTALL( FILES ${CMAKE_SOURCE_DIR}/admin/unix/tomahawk.desktop  RENAME ${TOMAHAWK_TARGET_NAME}.desktop DESTINATION ${CMAKE_INSTALL_DATADIR}/applications )
+
